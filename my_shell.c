bc6216ea5094318125b80a892cb216ae69b544e7
Arif Fikri <ariffikriahmadabas@airasia.com>
2023-11-07 15:28:02 +0800

    Initial commit

---
+++ my_shell.c
@@ -0,0 +1,11 @@
+include "types.h"
+include "stat.h"
+include "user.h"
+    
+ //passing command line arguments 
+    
+int main(int argc, char *argv[]) 
+{ 
+  printf(1, "Hello world\n"); 
+  exit(); 
+} 
